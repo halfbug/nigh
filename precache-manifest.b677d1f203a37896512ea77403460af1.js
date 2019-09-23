d52f3ddd9b6cc57122d372819de4207bc2a7c30f
Sadaf <sadaf_cu@hotmail.com>
2019-09-23 10:09:44 +0500

    Updates

---
+++ precache-manifest.b677d1f203a37896512ea77403460af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf92252bc7dedf19668e319ba41d1c3e",
+    "url": "/nigh/index.html"
+  },
+  {
+    "revision": "e0edeeea69f4d82be95e",
+    "url": "/nigh/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "07c77e0ce3132f877781",
+    "url": "/nigh/static/js/2.6fe3b3d0.chunk.js"
+  },
+  {
+    "revision": "e0edeeea69f4d82be95e",
+    "url": "/nigh/static/js/main.da825a0f.chunk.js"
+  },
+  {
+    "revision": "9e1c17726fcdd3afe9e4",
+    "url": "/nigh/static/js/runtime-main.33ba3fd7.js"
+  }
+]);
